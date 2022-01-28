eca5af7fb27d02a585af4b136a97200973f4aba0
Sean Bamford <sean.bamford.sb@gmail.com>
2022-01-28 16:10:14 -0500

    missing projects section

--- 10_Review_Day/exercise/TEams/Project.cs
+++ 10_Review_Day/exercise/TEams/Project.cs
@@ -5,6 +5,22 @@ using System.Text;
 namespace TEams
 {
     class Project
-    {
+    { //Proprties
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string StartDate { get; set; }
+        public string DueDate { get; set; }
+        public List<Employee> TeamMembers { get; set; }
+
+        //Constructor
+        public Project (string name, string description, string startDate, string dueDate)
+        {
+            Name = name;
+            Description = description;
+            StartDate = startDate;
+            DueDate = dueDate;
+
+        }
     }
 }
