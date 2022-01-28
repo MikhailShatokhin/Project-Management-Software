c964c57f06ae79df5c0a1631300abf847065b6ef
Misha Shatokhin <Shatokhingti@gmail.com>
2022-01-28 14:04:59 -0500

    Completed Step Two-MS

--- 10_Review_Day/exercise/TEams/Employee.cs
+++ 10_Review_Day/exercise/TEams/Employee.cs
@@ -6,5 +6,41 @@ namespace TEams
 {
     class Employee
     {
+        //Properties
+        public long EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; } = 60000;
+        public Department Department { get; set; }
+        public string HireDate { get; set; }
+        public string FullName 
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+        //Constructors
+        public Employee(long employeeId, string firstName, string lastName, string email, Department department, string hireDate)
+        {
+            EmployeeId = employeeId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Department = department;
+            HireDate = hireDate;
+        }
+        public Employee()
+        {
+
+        }
+        //Methods
+        public double RaiseSalary(double percent)
+        {
+            return Salary = (Salary * percent) + Salary;
+        }
+
+
     }
 }
